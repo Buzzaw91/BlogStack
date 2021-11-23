d3e76910c6707c006b82ff53477f16f09031a298
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-11-23 14:19:42 +0200

    style: post component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Post = ({title, url, body}) => {
 
     return (
-        <div className='container mx-auto flex-col justify-center items-center my-4 border max-w-xl'>
-            <img src={url} alt='post' className='w-72 h-36 ml-24' />
+        <div className='container mx-auto flex-col my-4 shadow max-w-xl hover:bg-blue-100'>
+            <img src={url} alt='post' className='w-72 h-36 ml-24 mt-6 flex justify-center' />
             <h1 className='font-robotoSlab text-2xl flex justify-center'>{title}</h1>
             <div className='font-sans text-md flex justify-center py-4 px-4'>{body}</div>
         </div>
