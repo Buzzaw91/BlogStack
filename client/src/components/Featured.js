be331b18ff02de4d667bf7d3b06993612000c575
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-11-23 13:40:14 +0200

    add Link to featured user component

--- client/src/components/Featured.js
+++ client/src/components/Featured.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Featured = ({ users }) => {
 
@@ -6,14 +7,16 @@ const Featured = ({ users }) => {
         <>
             <div className='container flex-col items-center mt-10 mb-4 my-8'>
                 {users.map((user) => (
-                    <div className='flex justify-center px-2 py-2 max-w-lg my-2'>
-                        <img className='mx-1 my-1 h-20 w-20 flex' src={user.avatar} alt='profile'/>
-                        <div className='flex-col ml-6'>
-                            <h1 className='flex font-semibold'>{user.blogName}</h1>
-                            <h3 className='flex'>{user.description}</h3>
-                            <h4 className='flex font-light py-2 text-sm'>By {user.username}</h4>
+                    <Link to={`/${user.username}`}>
+                        <div className='flex justify-center px-2 py-2 max-w-lg my-2'>
+                            <img className='mx-1 my-1 h-20 w-20 flex' src={user.avatar} alt='profile'/>
+                            <div className='flex-col ml-6'>
+                                <h1 className='flex font-semibold'>{user.blogName}</h1>
+                                <h3 className='flex'>{user.description}</h3>
+                                <h4 className='flex font-light py-2 text-sm'>By {user.username}</h4>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </>
