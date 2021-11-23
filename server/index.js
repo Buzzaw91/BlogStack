88d8ad7a551cc3fced9ae6a73e3f87de492989a6
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-11-23 17:36:20 +0200

    fix missing route

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 } else {
-    app.get('/', (req, res) => {
+    app.get('/api/v1/', (req, res) => {
         res.send('API is running...');
     });
 }
