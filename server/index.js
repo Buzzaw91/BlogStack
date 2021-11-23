5484de036ff04e609a5c709ec393cb37e7b33d25
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-11-23 18:02:34 +0200

    found the bug

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.use(errorHandler)
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 } else {
     app.get('/api/v1/', (req, res) => {
         res.send('API is running...');
