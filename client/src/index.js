46794eff530aeec1b1d3f0f2ab8784dff77e0117
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-11-25 15:16:45 +0200

    browser router location change

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>  
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
