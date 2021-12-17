b38511a629538c95bb6458daa48afca3f33e9908
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-12-17 11:31:26 +0200

    style: fix centering for featured users component

--- client/src/components/Featured.js
+++ client/src/components/Featured.js
@@ -8,11 +8,11 @@ const Featured = ({ users }) => {
             <div className='container flex-col items-center mt-10 mb-4 my-8'>
                 {users.map((user) => (
                     <Link to={`/${user.username}`}>
-                        <div className='flex justify-center px-2 py-2 max-w-lg my-2'>
+                        <div className='flex justify-center px-2 py-2 my-2 w-2/4'>
                             <img className='mx-1 my-1 h-20 w-20 flex' src={user.avatar} alt='profile'/>
                             <div className='flex-col ml-6'>
                                 <h1 className='flex font-semibold'>{user.blogName}</h1>
-                                <h3 className='flex'>{user.description}</h3>
+                                <h3 className='flex mx-2 py-1'>{user.description}</h3>
                                 <h4 className='flex font-light py-2 text-sm'>By {user.username}</h4>
                             </div>
                         </div>
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,9 +4,14 @@ const Profile = ({ user }) => {
 
     return (
         <div className='container mx-auto flex-col items-center max-w-screen-md font-robotoSlab border'>
+            <h1 className='flex justify-center text-2xl py-2'>{user.blogName}</h1>
+            <div className='flex justify-center'>
+                <img className='mx-1 my-1 h-20 w-20' src={user.avatar} alt='profile'/>
+            </div>
             <h1 className='flex justify-center text-2xl py-2'>{user.username}</h1>
-            <h1 className='flex justify-center text-md py-2'>{user.description}</h1>
-            <h1 className='flex justify-center text-md py-2'>{user.email}</h1>
+            <h3 className='flex justify-center text-sm py-2'>{user.bio}</h3>
+            <h1 className='flex justify-center text-sm py-2'>{user.description}</h1>
+            <h1 className='flex justify-center text-sm py-2'>{user.email}</h1>
         </div>
     )
 }
